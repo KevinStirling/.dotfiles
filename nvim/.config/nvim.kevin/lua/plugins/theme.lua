8115227437313993db849979d3c81311b576f244
Kevin Young <youngks93@gmail.com>
2026-05-05 21:30:48 -0400

    neovim .12 config rewrite

--- nvim/.config/nvim.kevin/lua/plugins/theme.lua
+++ nvim/.config/nvim.kevin/lua/plugins/theme.lua
@@ -1,1 +1,17 @@
-/home/kevin/.config/omarchy/current/theme/neovim.lua
+vim.pack.add({
+    'https://github.com/xiyaowong/nvim-transparent', -- transparent bg
+    'https://github.com/nvim-mini/mini.icons',       -- icons
+
+    -- themes
+    'https://github.com/TheNiteCoder/mountaineer.vim',
+    'https://github.com/sainnhe/gruvbox-material',
+})
+require("mini.icons").setup()
+
+vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_transparent_background = 2
+vim.g.gruvbox_material_foreground = 'material'
+vim.g.gruvbox_material_enable_bold = 1
+
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+vim.cmd.colorscheme('gruvbox-material')
